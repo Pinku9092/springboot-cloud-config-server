420a06fb9a66ffa5b535bfe3114f7735a02c0026
pinku <kpinku32@gmail.com>
2025-03-30 11:59:15 +0530

    first commit

---
+++ src/main/java/com/pinku/spring/boot/cloud/config/api/SpringbootCloudConfigServerApplication.java
@@ -0,0 +1,17 @@
+package com.pinku.spring.boot.cloud.config.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
+public class SpringbootCloudConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCloudConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pinku/spring/boot/cloud/config/api/SpringbootCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pinku.spring.boot.cloud.config.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
